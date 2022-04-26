4de44fca72ca98820c6a70effc24dd0cf32f51fc
smartcatdev <bilal@smartcat.ca>
2022-04-26 09:42:46 -0400

    added form submission from the frontend

--- public/scripts/frontend.js
+++ public/scripts/frontend.js
@@ -1,3 +1,4 @@
+
 jQuery(document).ready(function() {
 
     getAllTweets()
@@ -13,7 +14,7 @@ jQuery(document).ready(function() {
             let html = ''
 
             $(data).each(function(index,tweet) {
-                console.log(tweet.text)
+                // console.log(tweet.text)
                 html += '<div>' + tweet.text + '</div>'
             })
 
@@ -23,8 +24,48 @@ jQuery(document).ready(function() {
 
         })
     }
+    // End of GET API call
+    
 
-    // inject those tweets into the HTML Document
-    // DOM
+    // Post Tweets via API
+
+    // declare an event for the form submission
+    $('#post-tweet').submit(function(event) {
+        // prevent the form from reloading the page (default behaviour)
+        event.preventDefault()
+
+        // get the value of the text in the tweet text box
+        let tweet = $('.the-tweet').val()
+
+        // call the POST endpoint, and give it that data (request)
+        let payload = {
+            text: tweet
+        }
+
+        $.ajax({
+            url: 'http://localhost:3000/api/tweets',
+            method: 'POST',
+            data: JSON.stringify(payload),
+            contentType: 'application/json',
+            success: function(data) {
+                // when successful, reload the tweets showing the new one
+                getAllTweets()
+                
+                // empty the text box
+                $('.the-tweet').val('')
+
+                // post a success message to the user
+                $('#server-response').html(data.textResponse)
+            }
+        })
+    })
+
+
+
+
+    // when the submission is successful (response), update the list of tweets
+
+    //
+
 
 })
--- server.js
+++ server.js
@@ -24,7 +24,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 })
 
-
+/**
+ * Retrieve all tweets from the database
+ */
 app.get('/api/tweets', async (req, res) => {
     // connect to the database
     const db = await connectToDB()
@@ -32,11 +34,14 @@ app.get('/api/tweets', async (req, res) => {
     // grab the data from the collection
     const collection = await db.collection('tweets')
     const data = await collection.find({}).toArray()
-
+    
     // return the data as a JSON object
     res.json(data)
 })
 
+/**
+ * Posts tweet submissions, which get stored in the database
+ */
 app.post('/api/tweets', async (req, res) => {
     // connect to the database
     const db = await connectToDB()
@@ -49,7 +54,10 @@ app.post('/api/tweets', async (req, res) => {
     // store the tweet in the MongoDB database
     const tweet = await collection.insertOne({text})
     // return the tweet as a JSON object
-    res.send(tweet)
+    res.send({
+        status: true,
+        textResponse: 'Tweet has been inserted!'
+    })
 })
 
 
